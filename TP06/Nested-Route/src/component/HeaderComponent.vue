8ce31eb462cff14bf059dfe229a850d5377c666a
Chin Hongnyheng <hongnyhengchin@gmail.com>
2025-12-15 22:30:09 +0700

    Nested Route

---
+++ TP06/Nested-Route/src/component/HeaderComponent.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="container">
+        <h1 class="Header">Header</h1>
+        <div class="page">
+            <router-link to="/page1" class="mini-header">Page 1</router-link>
+            <router-link to="/page2" class="mini-header">Page 2</router-link>
+            <router-link to="/page3" class="mini-header">Page 3</router-link>
+        </div>
+    </div>
+</template>
+<script>
+export default{
+    name: 'HeaderComponent',
+}
+</script>
+<style scoped>
+.container{
+    width: 100%;
+    height: 200px;
+    border: 2px solid black;
+    display:flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 50px;
+    box-sizing: border-box;
+}
+.Header{
+    font-family: 'Nunito', sans-serif;
+    font-size: 24px;
+    font-weight: bold;
+}
+.page{
+    display:flex;
+    flex-direction: row;
+    justify-content: center;
+    padding: 0 50px;
+    gap: 50px;
+}
+.mini-header{
+    font-family: 'Nunito', sans-serif;
+    font-size: 24px;
+    font-weight: bold;
+    color: black;
+    text-decoration: none;
+}
+.router-link-active{
+    color: green;
+}
+</style>
---
+++ TP06/Nested-Route/src/views/Page2.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="page-container">
+        <span class="page2">Welcome to Page 2</span>
+        <SectionComponent v-if="sectionId" :section-id="sectionId" page-name="Page 2" />
+    </div>
+</template>
+<script>
+import SectionComponent from '@/component/SectionComponent.vue';
+import { useRoute } from 'vue-router';
+import { computed } from 'vue'
+
+export default{
+    name: 'Page2',
+    setup(){
+        const route = useRoute()
+
+        const sectionId = computed(() => route.params.SectionId)
+        return { sectionId }
+    },
+    components:{
+        SectionComponent,
+    },
+}
+</script>
+<style scoped>
+.page2{
+    font-family: sans-serif;
+    font-size: 36px;
+    font-weight: bold;
+}
+.page-container {
+  border: 1px solid black;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
